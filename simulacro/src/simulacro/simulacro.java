6742ec9744a63e6eb197367eb76b84c2423d3cea
Jesus <Jesus@DESKTOP-64CLO6L>
2022-11-29 17:32:37 +0100

    System.out.printl
    Se ha añadido un system.out.println con el texto hola

--- simulacro/src/simulacro/simulacro.java
+++ simulacro/src/simulacro/simulacro.java
@@ -2,4 +2,13 @@ package simulacro;
 
 public class simulacro {
 
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		
+		System.out.println("Hola");
+		
+		
+	}
 }
